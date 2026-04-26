bd005d77c993cbf17f7113f285d6496bbb58c59c
Dmitry Pavlov <dpavlov@entroforce.ru>
2026-04-26 21:57:05 +0300

    build-oldtools.bat + fixes for old tools to work

--- src/build-oldtools.bat
+++ src/build-oldtools.bat
@@ -1,6 +1,6 @@
 biber --tool references.bib --output-file=references-clones.bib --config=clones.conf --output-legacy-dates
-xelatex vg
-bibtexu vg 
-bibtexu eng 
-xelatex vg
-xelatex vg
+pdflatex vg
+bibtex vg 
+bibtex eng 
+pdflatex vg
+pdflatex vg
